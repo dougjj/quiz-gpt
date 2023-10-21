b0cbcf99276dc502da70bc1554d46d11ef308bf5
Doug Coulter <dougjcoulter@gmail.com>
2023-10-21 23:03:51 +1100

    api test

---
+++ app/api/test4/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from 'next/server' 
+
+
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export const dynamic = 'force-dynamic';
+
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+ 
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const prompt = searchParams.get('query') || "nothing"
+
+  // const supabase = createRouteHandlerClient<Database>({ cookies })
+  const cookieStore = cookies()
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+
+  const questions = await supabase.from('Question').select().filter('topic', 'eq', prompt);
+  return Response.json({questions});
+}
