88fdf66f4fe0ae63d6e342c4b73df1206237e6e0
Doug Coulter <dougjcoulter@gmail.com>
2023-10-22 19:02:28 +1100

    delete unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Search from '@/components/search'
 
 export const metadata = {
   title: 'Quizomatic',
-  description: 'Quizomatic is a quiz app that lets you create and take quizzes on any topic.',
+  description: 'Quizomatic is a quiz app that lets you create quizzes on any topic.',
 }
 
 export default function RootLayout({
