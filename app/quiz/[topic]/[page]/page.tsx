0cd45bb066d7a68f4f378c9e16db17af9cd56fba
Doug Coulter <dougjcoulter@gmail.com>
2023-10-25 01:03:14 +1100

    Page navigation

--- app/quiz/[topic]/[page]/page.tsx
+++ app/quiz/[topic]/[page]/page.tsx
@@ -4,11 +4,16 @@ import { cookies } from 'next/headers'
 import { Database } from '@/types/supabase';
 
 import NewQuestions from '@/components/new_questions';
-import { Stack, Typography } from '@mui/joy';
 import Search from '@/components/search';
 import Link from 'next/link';
 
-export const dynamic = 'force-dynamic';
+import Stack from '@mui/joy/Stack';
+import Typography from '@mui/joy/Typography';
+import Button from '@mui/joy/Button';
+import ButtonGroup from '@mui/joy/ButtonGroup';
+
+
+// export const dynamic = 'force-dynamic';
 
 export default async function Page({ params }: { params: { topic: string, page: number } }) {
     const topic = decodeURIComponent(params.topic);
@@ -19,18 +24,24 @@ export default async function Page({ params }: { params: { topic: string, page: 
     const from = (page - 1) * 10;
     const to = page * 10 - 1;
 
+    console.time("supabase")
+    
     const supabase = createRouteHandlerClient<Database>({ cookies })
-    const questions = await supabase.from('Question').select()
+    const [questions, count] = await Promise.all([supabase.from('Question').select()
         .filter('topic', 'eq', topic)
-        .range(from, to);
+        .range(from, to),
+        supabase.from('Question').select('id', {count : 'exact'})
+        .filter('topic', 'eq', topic),])
+    console.timeEnd("supabase")
+
+    const numPages = Math.ceil((count.count || 0) /10);
 
     console.log("topic, page: ", topic, page)
     console.log("page q length: ", questions.data?.length)
 
     if (questions.data?.length == 0) {
         return (
-            <QuizPage>
-                Client side generation
+            <QuizPage topic={topic} numPages={numPages}>
                 <NewQuestions key={topic} topic={topic} page={page}/>
             </QuizPage>
         )
@@ -39,7 +50,7 @@ export default async function Page({ params }: { params: { topic: string, page: 
     const questionsString = questions.data?.map(q => [q.question, JSON.stringify(q.options), q.answer, q.explanation].join("\n")).join("\n\n");
 
     return (
-        <QuizPage>
+        <QuizPage topic={topic} numPages={numPages}>
             <Quiz completion={questionsString || ""} 
                 isLoading={false}
                 topic={topic}
@@ -48,14 +59,46 @@ export default async function Page({ params }: { params: { topic: string, page: 
     )
   }
 
-function QuizPage({ children } : { children: React.ReactNode }) {
+function QuizPage({ topic, numPages, children } : { topic: string, numPages: number, children: React.ReactNode }) {
     return (
-        <Stack spacing={2}>
+        <Stack spacing={numPages}>
             <Link href={"/"}>
                 <Typography level="h4">Quizomatic</Typography>
             </Link>
             <Search />
             {children}
+            <PageSelector topic={topic} numPages={numPages} />
         </Stack>
     )
 };
+
+function PageSelector({ topic, numPages } : { topic: string, numPages: number}) {
+    const nPages = Math.min(10, numPages);
+    return (
+        <ButtonGroup spacing={1}>
+            {Array.from(Array(nPages).keys()).map((i) =>(
+                <PageSelectorButton key={i} topic={topic} page={i+1}/>
+            )
+            )}
+        </ButtonGroup>
+    )
+};
+
+function PageSelectorButton({ topic, page } : { topic: string, page: number}) {
+    return (
+        <Button 
+            component={Link}
+            href={`/quiz/${topic}/${page}`}>
+            <Typography level="body-sm">
+                {page}    
+            </Typography>
+        </Button>
+    )
+};
+
+export async function generateStaticParams() {  
+    return ["shakespeare"].map((topic) => ({
+      topic: topic,
+      page: "1",
+    }))
+  }
